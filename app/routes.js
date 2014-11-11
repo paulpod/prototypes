8b0cb615aceee73bbc350caefd6d3d54efdac3ed
paulpod <paulpod@neuromantics.net>
2014-11-11 13:15:57 +0000

    last bits of change address - add V5C + VRM numbers to update keeper

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,10 @@ module.exports = {
                 {'assetPath' : assetPath});
     });
 
+    
+
+
+    /* - - - - - - - */
     /* Example pages */
 
     app.get('/examples/hello-world', function (req, res) {
@@ -18,6 +22,55 @@ module.exports = {
     app.get('/examples/alpha', function (req, res) {
       res.render('examples/alpha/alpha', {'assetPath' : assetPath });    
     });
+
+
+
+
+    /* - - - - - - - - - - - - - - - - */
+    /* Pages for Dart Charge prototype */
+
+    app.get('/local-type', function (req, res) {
+      var o = req.query.Option;
+      
+      //res.render('local-type-' + o, {'assetPath' : assetPath });
+      res.redirect('https://test.dart-charge.co.uk/Registration/RegisterLocalResident?option=' + o);
+
+    });
+
+
+    app.get('/account-type', function (req, res) {
+      var o = req.query.Option;
+
+      if (o == 'personal') {
+        res.redirect('https://test.dart-charge.co.uk/Registration/Register');
+      } else if (o == 'local') {
+        res.render('dart-account-1-' + o, {'assetPath' : assetPath });
+      } else if (o == 'company') {
+        res.redirect('https://test.dart-charge.co.uk/Registration/RegisterBusiness');
+      }
+      
+    });
+
+
+
+
+    /* - - - - - - - - - - - - - - */
+    /* Pages for Change of address */
+
+    app.get('/also-change-keeper', function (req, res) {
+
+    var x = req.query.keepertoo;
+
+    if (x == 'yes') {
+      res.render('v5c-reg-lookup', {'assetPath' : assetPath});
+    } else if (x == 'no') {
+      res.render('finish', {'assetPath' : assetPath});
+    }
+
+})
+
+
+
 
   }
 };
